c6966db0bfa23be0fdced08a2b1542056bc4352a
BenjikQ <benjik1656@gmail.com>
2022-12-04 12:46:12 +0100

    Fix formatting

--- day04/day04_lib.cpp
+++ day04/day04_lib.cpp
@@ -68,19 +68,19 @@ struct Range {
 
 int countOverlapping(const std::vector<std::string>& data) {
     auto rng = data
-               | ranges::view::transform([](const auto& s) {
-        const auto assignments = s
-                                 | ranges::view::split(',')
-                                 | ranges::view::transform([](const auto& assignment) {
-            auto&& r = assignment | ranges::view::split('-');
-            return r | ranges::view::transform([](const auto& p) {
-                return *toInt(p | ranges::to<std::string>);
-            });
-        })
-                                 | ranges::to<std::vector<std::vector<int>>>;
-        Range r1{ assignments[0][0], assignments[0][1] };
-        Range r2{ assignments[1][0], assignments[1][1] };
-        return std::pair{ r1, r2 };
+           | ranges::view::transform([](const auto& s) {
+                const auto assignments = s
+                 | ranges::view::split(',')
+                 | ranges::view::transform([](const auto& assignment) {
+                    auto&& r = assignment | ranges::view::split('-');
+                    return r | ranges::view::transform([](const auto& p) {
+                        return *toInt(p | ranges::to<std::string>);
+                    });
+                })
+                 | ranges::to<std::vector<std::vector<int>>>;
+            Range r1{ assignments[0][0], assignments[0][1] };
+            Range r2{ assignments[1][0], assignments[1][1] };
+            return std::pair{ r1, r2 };
     });
     return ranges::count_if(rng, [](const auto& assignments) {
         return overlap(assignments.first, assignments.second);
